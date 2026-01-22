860da57edeca73014b0823e0ef1f98a1471b8dfe
EtienneVtr <etienne.vatry@telecomnancy.net>
2026-01-22 10:13:02 +0100

    liquid glass v2

--- ui.cpp
+++ ui.cpp
@@ -118,6 +118,68 @@ static void DrawReflection(HDC hdc, int barW, int barH, int centerX, int alpha) 
     DeleteDC(memDC);
 }
 
+static void DrawTopHighlight(HDC hdc, int w, int h, int alpha) {
+    if (alpha <= 0) return;
+
+    int hh = (int)(h * 0.38f); // hauteur de highlight
+    if (hh < 6) hh = 6;
+
+    BITMAPINFO bmi{};
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = w;
+    bmi.bmiHeader.biHeight = -hh;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    void* bits = nullptr;
+    HDC memDC = CreateCompatibleDC(hdc);
+    HBITMAP dib = CreateDIBSection(memDC, &bmi, DIB_RGB_COLORS, &bits, nullptr, 0);
+    HGDIOBJ old = SelectObject(memDC, dib);
+
+    auto* px = (unsigned int*)bits;
+
+    for (int y = 0; y < hh; y++) {
+        // fort tout en haut, puis fade très rapide
+        float t = 1.0f - (float)y / (float)(hh - 1);
+        t = powf(t, 2.2f);
+
+        int a = (int)(alpha * t);
+        if (a > 255) a = 255;
+
+        unsigned int v = (unsigned int)((a << 24) | (a << 16) | (a << 8) | a);
+        for (int x = 0; x < w; x++) px[y * w + x] = v;
+    }
+
+    BLENDFUNCTION bf{ AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };
+    AlphaBlend(hdc, 0, 0, w, hh, memDC, 0, 0, w, hh, bf);
+
+    SelectObject(memDC, old);
+    DeleteObject(dib);
+    DeleteDC(memDC);
+}
+
+static void DrawBevel(HDC hdc, int w, int h) {
+    HPEN light = CreatePen(PS_SOLID, 1, RGB(160, 160, 160));
+    HPEN dark = CreatePen(PS_SOLID, 1, RGB(35, 35, 35));
+
+    HGDIOBJ old = SelectObject(hdc, light);
+
+    // haut + gauche
+    MoveToEx(hdc, 1, 1, NULL); LineTo(hdc, w - 2, 1);
+    MoveToEx(hdc, 1, 1, NULL); LineTo(hdc, 1, h - 2);
+
+    SelectObject(hdc, dark);
+
+    // bas + droite
+    MoveToEx(hdc, 1, h - 2, NULL); LineTo(hdc, w - 2, h - 2);
+    MoveToEx(hdc, w - 2, 1, NULL); LineTo(hdc, w - 2, h - 2);
+
+    SelectObject(hdc, old);
+    DeleteObject(light);
+    DeleteObject(dark);
+}
+
 void DrawUI(HWND hwnd, HDC hdc) {
     RECT rc;
     GetClientRect(hwnd, &rc);
@@ -125,33 +187,25 @@ void DrawUI(HWND hwnd, HDC hdc) {
 
     int barW = rc.right - rc.left;
     int barH = rc.bottom - rc.top;
+
+    // 1) highlight du haut (effet verre)
+    DrawTopHighlight(hdc, barW, barH, 70);
+
+    // 2) reflet qui suit la souris
     DrawReflection(hdc, barW, barH, 0, reflectAlpha);
 
-    HPEN hTop = CreatePen(PS_SOLID, 1, RGB(170, 170, 170));
-    HPEN hBot = CreatePen(PS_SOLID, 1, RGB(40, 40, 40));
-
-    HGDIOBJ oldPen = SelectObject(hdc, hTop);
-    MoveToEx(hdc, 1, 1, NULL);
-    LineTo(hdc, barW - 2, 1);
-
-    SelectObject(hdc, hBot);
-    MoveToEx(hdc, 1, barH - 2, NULL);
-    LineTo(hdc, barW - 2, barH - 2);
-
-    SelectObject(hdc, oldPen);
-    DeleteObject(hTop);
-    DeleteObject(hBot);
-
+    // 3) icônes
     for (auto& app : apps) {
         DrawIconEx(hdc, app.rect.left, app.rect.top, app.icon, iconSize, iconSize, 0, NULL, DI_NORMAL);
     }
 
+    // 4) bouton close
     if (closeAlpha > 0) {
         int a = closeAlpha;
         COLORREF col = RGB((120 * a) / 255, (120 * a) / 255, (120 * a) / 255);
 
         HPEN hPen = CreatePen(PS_SOLID, 2, col);
-        HGDIOBJ oldPen = SelectObject(hdc, hPen);
+        HGDIOBJ oldPen2 = SelectObject(hdc, hPen);
 
         MoveToEx(hdc, closeBtnRect.left, closeBtnRect.top, NULL);
         LineTo(hdc, closeBtnRect.right, closeBtnRect.bottom);
@@ -159,9 +213,12 @@ void DrawUI(HWND hwnd, HDC hdc) {
         MoveToEx(hdc, closeBtnRect.right, closeBtnRect.top, NULL);
         LineTo(hdc, closeBtnRect.left, closeBtnRect.bottom);
 
-        SelectObject(hdc, oldPen);
+        SelectObject(hdc, oldPen2);
         DeleteObject(hPen);
     }
+
+    // 5) bevel / rim par-dessus (finish)
+    DrawBevel(hdc, barW, barH);
 }
 
 void OnLeftButtonDown(HWND hwnd, int x, int y) {
